a84a7e3016b73176ce31f8143db0ef00e588dc03
24moto <aveeee18@gmail.com>
2023-02-17 15:08:24 +0900

    デプロイエラー対応

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,6 @@ class UsersController < ApplicationController
   def show
     user = User.find(params[:id])
     @nickname = current_user.nickname
-    @records = current_user.records
-    # @records = current_user.records.order('date DESC')
+    @records = current_user.records.order('date DESC')
   end
 end
